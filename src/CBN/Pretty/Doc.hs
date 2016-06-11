938ac0ca78b874c55a1f6263e740551200789556
Edsko de Vries <edsko@well-typed.com>
2016-06-11 13:57:23 +0800

    Move precedence stuff to its own module

--- src/CBN/Pretty/Doc.hs
+++ src/CBN/Pretty/Doc.hs
@@ -8,6 +8,7 @@ import qualified Data.Map as Map
 
 import CBN.Language
 import CBN.Heap
+import CBN.Pretty.Precedence
 
 instance Pretty Var where pretty (Var x) = text x
 instance Pretty Con where pretty (Con c) = text c
@@ -73,46 +74,3 @@ instance Pretty a => Pretty (Heap a) where
 parensIf :: Bool -> Doc -> Doc
 parensIf False = id
 parensIf True  = parens
-
-{-------------------------------------------------------------------------------
-  Dealing with precedence
-
-  Adapted from
-  <https://mail.haskell.org/pipermail/haskell-cafe/2008-January/038501.html>.
--------------------------------------------------------------------------------}
-
-data Operator        = Ap | Lam | Let | Case              deriving Eq
-data Assoc           = AssocLeft | AssocRight | AssocNone deriving Eq
-data FixityContext   = Top | L Operator | R Operator
-type PartialOrdering = Maybe Ordering
-
-assoc :: Operator -> Assoc
-assoc Ap   = AssocLeft
-assoc Lam  = AssocRight
-assoc Case = AssocRight
-assoc Let  = AssocRight
-
-comparePrec :: Operator -> Operator -> PartialOrdering
-comparePrec op1 op2 | op1 == op2 = Just EQ
-comparePrec Ap  _   = Just GT
-comparePrec _   Ap  = Just LT
-comparePrec _   _   = Just EQ
-
-needsParens :: FixityContext -> Operator -> Bool
-needsParens Top _  = False
-needsParens (L ctxt) op
-    | comparePrec ctxt op == Just LT = False
-    | comparePrec ctxt op == Just GT = True
-    | comparePrec ctxt op == Nothing = True
-    -- otherwise the two operators have the same precedence
-    | assoc ctxt /= assoc op         = True
-    | assoc ctxt == AssocLeft        = False
-    | otherwise                      = True
-needsParens (R ctxt) op
-    | comparePrec ctxt op == Just LT = False
-    | comparePrec ctxt op == Just GT = True
-    | comparePrec ctxt op == Nothing = True
-    -- otherwise the two operators have the same precedence
-    | assoc ctxt /= assoc op         = True
-    | assoc ctxt == AssocRight       = False
-    | otherwise                      = True
---
+++ src/CBN/Pretty/Precedence.hs
@@ -0,0 +1,45 @@
+-- | Dealing with precedence
+--
+-- Adapted from
+-- <https://mail.haskell.org/pipermail/haskell-cafe/2008-January/038501.html>.
+module CBN.Pretty.Precedence (
+    FixityContext(..)
+  , Operator(..)
+  , needsParens
+  ) where
+
+data Operator        = Ap | Lam | Let | Case              deriving Eq
+data Assoc           = AssocLeft | AssocRight | AssocNone deriving Eq
+data FixityContext   = Top | L Operator | R Operator
+type PartialOrdering = Maybe Ordering
+
+assoc :: Operator -> Assoc
+assoc Ap   = AssocLeft
+assoc Lam  = AssocRight
+assoc Case = AssocRight
+assoc Let  = AssocRight
+
+comparePrec :: Operator -> Operator -> PartialOrdering
+comparePrec op1 op2 | op1 == op2 = Just EQ
+comparePrec Ap  _   = Just GT
+comparePrec _   Ap  = Just LT
+comparePrec _   _   = Just EQ
+
+needsParens :: FixityContext -> Operator -> Bool
+needsParens Top _  = False
+needsParens (L ctxt) op
+    | comparePrec ctxt op == Just LT = False
+    | comparePrec ctxt op == Just GT = True
+    | comparePrec ctxt op == Nothing = True
+    -- otherwise the two operators have the same precedence
+    | assoc ctxt /= assoc op         = True
+    | assoc ctxt == AssocLeft        = False
+    | otherwise                      = True
+needsParens (R ctxt) op
+    | comparePrec ctxt op == Just LT = False
+    | comparePrec ctxt op == Just GT = True
+    | comparePrec ctxt op == Nothing = True
+    -- otherwise the two operators have the same precedence
+    | assoc ctxt /= assoc op         = True
+    | assoc ctxt == AssocRight       = False
+    | otherwise                      = True
